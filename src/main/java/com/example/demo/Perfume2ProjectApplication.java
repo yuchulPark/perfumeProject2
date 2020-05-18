39635ec5bfa68c469e194ca13983592449d6cc5c
park yu chul <pycupper@naver.com>
2020-05-18 11:48:26 +0900

    유철) 향수 포트폴리오

---
+++ src/main/java/com/example/demo/Perfume2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Perfume2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Perfume2ProjectApplication.class, args);
+	}
+
+}
